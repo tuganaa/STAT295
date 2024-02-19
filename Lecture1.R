6ff23d65fbc11325e58ed735153e424c07886bb4
tuganaa <tuganakn@gmail.com>
2024-02-19 15:25:19 +0300

    ahshhaaa

--- Lecture1.R
+++ Lecture1.R
@@ -7,3 +7,7 @@ now <- Sys.time() #şuan zamanını verir.
 now
 
 install.packages(c("devtools","roxygen2","testthat","rmarkdown","pkgdown","purrr"))
+
+usethis::create_github_token()
+
+gitcreds::gitcreds_set()
